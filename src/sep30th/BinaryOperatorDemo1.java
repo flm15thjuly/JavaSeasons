b0037679e4b72b4d883c7338f9b0c7743c913096
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-10-01 20:23:52 +0530

    Java Season 2 Notes and Programs till Sep 30th

---
+++ src/sep30th/BinaryOperatorDemo1.java
@@ -0,0 +1,19 @@
+package sep30th;
+
+public class BinaryOperatorDemo1 {
+
+	public static void main(String[] args) {
+		
+		int x=20;
+		
+		//int y=x++;
+		//int y=x--;
+		//int y=++x;
+		int y=--x;
+		
+		System.out.println(y);
+		System.out.println(x);
+
+	}
+
+}
---
+++ src/sep30th/BinaryOperatorDemo2.java
@@ -0,0 +1,20 @@
+package sep30th;
+
+public class BinaryOperatorDemo2 {
+
+	public static void main(String[] args) {
+		
+	 char x=97;
+		
+	//	double x=3.14;
+		
+		//boolean x=true;
+	 
+	  x++;
+	  
+	  System.out.println(x);
+			 
+		
+	}
+
+}
